f9d9433cc0288ef32358db39a012bfe3279ac7fb
Arpan Tyagi <arpantyagiat@outlook.com>
2025-01-10 23:25:55 +0100

    Initial Commit

---
+++ TheOfficeAPI/TheOfficeAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheOfficeAPIApp.swift
+//  TheOfficeAPI
+//
+//  Created by Arpan Tyagi on 10.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TheOfficeAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TheOfficeAPITests/TheOfficeAPITests.swift
@@ -0,0 +1,16 @@
+//
+//  TheOfficeAPITests.swift
+//  TheOfficeAPITests
+//
+//  Created by Arpan Tyagi on 10.01.2025.
+//
+
+import Testing
+
+struct TheOfficeAPITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
